3ecd7f7a7a0fdb48645035285f20989cf9c8e9ca
laskal05 <hrh.syn.mnmk.lb@gmail.com>
2019-09-14 11:00:22 +0900

    feat: add sample code of cognito client
    Signed-off-by: laskal05 <hrh.syn.mnmk.lb@gmail.com>

---
+++ cognito/client_node/signin.js
@@ -0,0 +1,33 @@
+global.fetch = require("node-fetch");
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+const config = require("./config");
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(config.poolData);
+const userData = {
+  Username: config.username,
+  Pool: userPool
+};
+
+const authenticationData = {
+  Username: config.username,
+  Password: config.password
+};
+const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
+  authenticationData
+);
+
+const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+cognitoUser.authenticateUser(authenticationDetails, {
+  onSuccess: function(result) {
+    var accessToken = result.getAccessToken().getJwtToken();
+    console.log(accessToken);
+  },
+
+  onFailure: function(err) {
+    console.error(err);
+  },
+  mfaRequired: function(codeDeliveryDetails) {
+    var verificationCode = prompt("Please input verification code", "");
+    cognitoUser.sendMFACode(verificationCode, this);
+  }
+});
---
+++ cognito/client_node/signup.js
@@ -0,0 +1,38 @@
+global.fetch = require("node-fetch");
+const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
+const config = require("./config");
+
+/* Create object for User Pool */
+const userData = {
+  Username: config.username,
+  Pool: new AmazonCognitoIdentity.CognitoUserPool({
+    UserPoolId: config.UserPoolId
+    ClientId: config.ClientId
+  });
+};
+
+/* Create Attribute */
+let attributeList = [];
+const dataEmail = {
+  Name: "email",
+  Value: config.email
+};
+attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail));
+
+const dataPhoneNumber = {
+  Name: "phone_number",
+  Value: config.phone
+};
+attributeList.push(
+  new AmazonCognitoIdentity.CognitoUserAttribute(dataPhoneNumber)
+);
+
+/* Signup */
+userPool.signUp(config.username, config.password, attributeList, null, (err, result) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const cognitoUser = result.user;
+  console.log("user name is " + cognitoUser.getUsername());
+});
